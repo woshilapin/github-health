74cbb57adc68ead4deb6b050554856fffff70525
Jean SIMARD <woshilapin@tuziwo.info>
2015-07-16 15:20:57 +0200

    Improve graphics for # of PR over time

--- r/number-of-pull-requests-over-time.r
+++ r/number-of-pull-requests-over-time.r
@@ -3,7 +3,10 @@ source("Rinit");
 year <- year(data.pulls$created_at);
 month <- month(data.pulls$created_at);
 
-pr_number_over_time <- aggregate(created_at ~ month + year, data.pulls, FUN=length);
-png(filename="graphics/number-of-pull-requests-over-time.png", width=800, height=600, units="px");
-barplot(pr_number_over_time$created_at);
+pr_number_over_time.month <- aggregate(created_at ~ month + year, data.pulls, FUN=length);
+pr_number_over_time.year <- aggregate(created_at ~ year, data.pulls, FUN=length);
+png(filename="graphics/number-of-pull-requests-over-time.month.png", width=800, height=600, units="px");
+barplot(pr_number_over_time.month$created_at);
+png(filename="graphics/number-of-pull-requests-over-time.year.png", width=800, height=600, units="px");
+barplot(pr_number_over_time.year$created_at);
 dev.off();
