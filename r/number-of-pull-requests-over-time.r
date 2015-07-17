b39f965390c69b37972f17949a31b1e58e47fd30
Jean SIMARD <woshilapin@tuziwo.info>
2015-07-18 00:47:37 +0200

    Improve number of PR

--- r/number-of-pull-requests-over-time.r
+++ r/number-of-pull-requests-over-time.r
@@ -6,8 +6,14 @@ month <- month(data.pulls$created_at);
 
 pr_number_over_time.month <- aggregate(created_at ~ month + year, data.pulls, FUN=length);
 pr_number_over_time.year <- aggregate(created_at ~ year, data.pulls, FUN=length);
+row.names(pr_number_over_time.month) <- paste(pr_number_over_time.month$month, pr_number_over_time.month$year);
+row.names(pr_number_over_time.year) <- pr_number_over_time.year$year;
+pr_number_over_time.month <- pr_number_over_time.month[, c(3)];
+pr_number_over_time.year <- pr_number_over_time.year[, c(2)];
 png(filename="graphics/number-of-pull-requests-over-time.month.png", width=800, height=600, units="px");
-barplot(pr_number_over_time.month$created_at);
+month.plot <- barplot(pr_number_over_time.month, names.arg=unique(paste(month, year)), ylim=c(0,55));
+text(month.plot, pr_number_over_time.month, label=pr_number_over_time.month, col="blue", pos=3, offset=.5);
 png(filename="graphics/number-of-pull-requests-over-time.year.png", width=800, height=600, units="px");
-barplot(pr_number_over_time.year$created_at);
+year.plot <- barplot(pr_number_over_time.year, names.arg=unique(year), ylim=c(0,220));
+text(year.plot, pr_number_over_time.year, label=pr_number_over_time.year, col="blue", pos=3, offset=.5);
 dev.off();
